1616ab56f7851be63685ef795327c1aae5d6ca61
sideffectt <okankgunduz25@gmail.com>
2025-12-18 16:09:08 +0300

    Initial Commit

---
+++ PrivacyAudit/PrivacyAuditApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivacyAuditApp.swift
+//  PrivacyAudit
+//
+//  Created by sideffect on 18.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct PrivacyAuditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
